45d91e6e6ca6b9fbc9f16d1984521391f2a64474
foospidy <foospidy@www.foospidy.com>
2015-04-29 19:20:58 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,89 @@
+// XSSwat
+// 
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function(request) {
+		try {
+			if('GET' == request.method) {
+				console.log("url: " + request.url);
+				console.log("method: " + request.method);
+				
+				var request_signature = '';
+				var query_parameters  = '';
+				var hash_parameters   = '';
+				var hash_signature    = '';
+
+				// get query parameters
+				var url_parts = request.url.split('?', 2);
+				
+				if(2 == url_parts.length) {
+					request_signature = url_parts[0];
+					query_parameters  = url_parts[1];
+
+					var parameters = query_parameters.split('&');
+					//console.log(parameters.length);
+					
+					for(i=0; i<parameters.length; i++) {
+						//console.log('    ' + parameters[i]);
+						parameter_parts = parameters[i].split('=', 2);
+						request_signature += '?' + parameter_parts[0];						
+					}
+				}
+				
+				// get hash parameters
+				var url_parts = request.url.split('#', 2);
+				
+				if(2 == url_parts.length) {
+					// if there were no query parameters then set url base to request signature
+					if(!request_signature.length) {
+						request_signature = url_parts[0];
+					}
+					
+					hash_parameters = url_parts[1];
+
+					var parameters = hash_parameters.split('&');
+					//console.log(parameters.length);
+					
+					for(i=0; i<parameters.length; i++) {
+						//console.log('    ' + parameters[i]);
+						parameter_parts = parameters[i].split('=', 2);
+						request_signature += '#' + parameter_parts[0];
+					}
+				}
+				
+				// if there were no query/hash parameters then set url base to request signature
+				if(!request_signature.length) {
+					request_signature = url_parts[0];
+				}
+					
+				console.log(request_signature);
+				var hash = CryptoJS.MD5(request_signature.trim());
+				console.log(hash.toString(CryptoJS.enc.Hex));
+				
+				xhr = new XMLHttpRequest();
+				xhr.open('GET', 'https://riskdiscovery.com/api/xsswat/' + hash, false);
+				xhr.send();
+				
+				json   = xhr.responseText;
+				object = JSON.parse(json);
+				
+				console.log(object.xss);
+				
+				if('true' == object.xss) {
+					return { redirectUrl: 'https://riskdiscovery.com/xsswat/' + hash } 
+				}
+			}
+
+			return { cancel: false }
+
+		} catch(e) {
+			console.log('Error: ' + e);
+			return { cancel: true }
+		}
+	}, {
+		urls: [
+		  "*://*/*"
+		]
+	},
+	["requestBody", "blocking"]	
+);
